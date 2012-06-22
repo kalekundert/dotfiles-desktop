2e070476b75fa772f1b7c44a69e7cd6be07a80b4
Kale Kundert <kale@theKunderts.net>
2012-06-22 10:36:23 -0700

    Move the IMP path to a local file.

--- setup/11.variables.sh
+++ setup/11.variables.sh
@@ -6,9 +6,7 @@ export MAN_PATH=~/hacking/installs/man
 export LD_LIBRARY_PATH=~/research/sali/imp/debug/build/lib/
 
 export PYTHONPATH=~/hacking:~/hacking/installs/lib/python2.7/site-packages
-export PYTHONPATH=~/research/sali/imp/debug/build/lib/:$PYTHONPATH
 
-export PYTHONSTARTUP=~/.pythonrc
 export CLASSPATH=.:./bin/:usr/share/java
 export TEXINPUTS=~/setup/desktop/latex:
 
