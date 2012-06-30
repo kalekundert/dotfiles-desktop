a5a0f3ebafd42cc5f7e68e3b4aa8919287b1be34
Kale Kundert <kale@thekunderts.net>
2012-06-29 22:53:46 -0700

    Dynamically resize the $PS2 prompt to match $PS1.

--- setup/14.prompt.sh
+++ setup/14.prompt.sh
@@ -1,6 +1,19 @@
+# Future Directions
+# =================
+# 1. I think zsh has builtin module for handling both colors and prompts.  Both
+#    of these are worth looking into.  
+#
+# 2. It's possible to put a prompt on the right-hand side of each line.  This
+#    is an interesting concept, because there's usually plenty of space over
+#    there.  Furthermore, on long lines, the right hand prompt will get
+#    overwritten.
+#
+#    It might be worth considering that this effect would be difficult to
+#    duplicate in ipython.
+
 # Define a number of colors using the standard shell escape codes.  The color
 # that gets used will depend on the role of this machine.  Note that these
-# colors will only work for fairly modern shells.
+# colors will only work in fairly modern terminals.
 
 normal=$'%{\e[0m%}'
 
@@ -22,26 +35,38 @@ remote_color=$blue
 # Define a few different prompt templates.  The template that gets used will
 # also depend on the role of this computer.
 
-basic='[%c] '
-remote='[%c] %n@%m: '
-continued=' '
+[ $USER = $HOME_USER ] && [ $HOST = $HOME_HOST ] && prompt='[%c] '
+[ $USER = $HOME_USER ] && [ $HOST != $HOME_HOST ] && prompt='[%c] %m: '
+[ $USER != $HOME_USER ] && [ $HOST = $HOME_HOST ] && prompt='[%c] %n: '
+[ $USER != $HOME_USER ] && [ $HOST != $HOME_HOST ] && prompt='[%c] %n@%m: '
 
 # Choose a color and a prompt string.  Both of these decisions depend on which
 # user is logged in and where they are logged in from.
 
-[[ -n $SSH_CONNECTION ]] && prompt=$remote || prompt=$basic
-[[ -n $SSH_CONNECTION ]] && color=$blue || color=$yellow
-
-[[ `/usr/bin/whoami` = 'root' ]] && color=$red
+[ $HOST = $HOME_HOST ] && color=$yellow || color=$blue
+[[ $USER = 'root' ]] && color=$red
 
 # Set the two variables that are used to display the prompt.
 
 PS1=$color$prompt$normal
-PS2=$color$continued$normal
+
+function precmd {
+
+    # This returns the size of the current prompt, accounting for all of the
+    # various escape codes.  I have no idea how it works.  If you really want
+    # to know, the manual page for `zshexpn' might be a good place to start
+    # looking.
+
+    local zero='%([BSUbfksu]|([FB]|){*})'
+    length=${#${(S%%)PS1//$~zero/}} 
+    padding=${(l.(($length - 4)).. .)}'... '
+
+    PS2=$color$padding$normal
+}
 
 # Add an extra control sequence to have the prompt string echoed in the
 # terminal's title bar.
 
 if [[ $TERM != "linux" ]] ; then
-    PS1=$'%{\e]0;%~\007%}'${PS1}
+    PS1=$'%{\e]0;%~\007%}'$PS1
 fi
