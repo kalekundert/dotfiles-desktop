2c6e0acadc3ff1a83c678549a361f5c0e7756af3
Kale Kundert <kale@thekunderts.net>
2012-07-12 22:49:43 -0700

    Add lost+found to ls ignore list.

--- setup/13.aliases.sh
+++ setup/13.aliases.sh
@@ -6,7 +6,8 @@ function ls () {
         --hide="*.log"                                                      \
         --hide="*.nlo"                                                      \
         --hide="*.bbl"                                                      \
-        --hide="*.blg"
+        --hide="*.blg"                                                      \
+        --hide="lost+found"
 }
 
 alias l='ls'
