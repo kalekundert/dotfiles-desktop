028bbbc50ba12d999eab9f9c7596f51cdeeab969
Kale Kundert <kale@thekunderts.net>
2013-04-17 13:04:22 -0700

    Add network and monitor aliases.

--- helpers/13.aliases.sh
+++ helpers/13.aliases.sh
@@ -10,11 +10,15 @@ alias top='\top -u $USER'
 alias topall='\top'
 alias date='\date +"%A, %B %-d%n%-I:%M %p"'
 alias where='whereis'
+alias updatedb='sudo updatedb'
 
 alias pu='pushd'
 alias po='popd'
 alias back='cd ~-'
 alias here='cd -P .'
+
+alias network='fork wicd-client -n'
+alias monitor='fork lxrandr'
 
 # In some directories, I like to have more control over what 'ls' displays.  
 # For example, sometimes I like to sort files alphabetically and sometimes I 
@@ -188,6 +192,4 @@ alias ssh-mountainview-proxy='autossh -fND 9050 mountainview-proxy'
 
 alias pg='ping google.com'
 alias pk='ping kxgames.net'
-
-alias ucsf-wpa='nmcli con up id "UCSF (secure)"'
 
