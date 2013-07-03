648a5eca89c4503adcf72f20a91b7fc2328c5c52
Kale Kundert <kale@thekunderts.net>
2013-07-03 09:21:45 -0700

    Set path to all subdirectories of ~/.local/bin

--- helpers/12.variables.sh
+++ helpers/12.variables.sh
@@ -1,18 +1,23 @@
-if [[ $PATH != *hacking/scripts* ]]; then
-    export PATH=~/.local/bin:$PATH
-    export PATH=~/hacking/scripts:$PATH
-    export PATH=~/hacking/installs/bin:$PATH
-    export PATH=~/hacking/installs/sbin:$PATH
-    export PATH=~/research/software/scripts:$PATH
-    export PATH=~/research/software/installs/bin:$PATH
-    export PATH=~/research/software/installs/sbin:$PATH
-fi
+#if [[ $PATH != *hacking/scripts* ]]; then
+#    export PATH=~/.local/bin:$PATH
+#    export PATH=~/hacking/scripts:$PATH
+#    export PATH=~/hacking/installs/bin:$PATH
+#    export PATH=~/hacking/installs/sbin:$PATH
+#    export PATH=~/research/software/scripts:$PATH
+#    export PATH=~/research/software/installs/bin:$PATH
+#    export PATH=~/research/software/installs/sbin:$PATH
+#fi
+
+export PATH='/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin'
+for directory in ~/.local/bin ~/.local/bin/*/; do
+    export PATH=$directory:$PATH
+done
 
 export EDITOR=vim
 export BROWSER=firefox
 
 export MAN_PATH=~/hacking/installs/man
-export PYTHONPATH=~/hacking/libraries:~/research/software/libraries
+export PYTHONPATH=~/.local/lib/python2.7/site-packages:~/hacking/libraries:~/research/software/libraries
 export CLASSPATH=.:./bin/:usr/share/java
 export TEXINPUTS=~/hacking/config/latex:
 export BSTINPUTS=~/hacking/config/latex:
