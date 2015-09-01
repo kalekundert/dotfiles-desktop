91d1762f2149567e34f172c371b1c98601b32478
Kale Kundert <kale@thekunderts.net>
2015-09-01 09:36:43 -0700

    Update PYTHONPATH and always set ls colors.

--- helpers/12.variables.sh
+++ helpers/12.variables.sh
@@ -8,19 +8,31 @@
 #    export PATH=~/research/software/installs/sbin:$PATH
 #fi
 
-export PATH='/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin'
-for directory in ~/.local/bin ~/.local/bin/*/; do
-    export PATH=$directory:$PATH
-done
+if [ -z "$PATH_CONFIGURED" ]; then
+    export PATH_CONFIGURED='yes'
+
+    export PATH="/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin"
+    export PATH="/home/kale/hacking/projects/android-sdk/platform-tools:$PATH"
+    export PATH="/home/kale/hacking/projects/android-sdk/tools:$PATH"
+    for directory in ~/.local/bin ~/.local/bin/*/; do
+        export PATH=$directory:$PATH
+    done
+
+    export MANPATH=~/.local/man:$MANPATH
+fi
 
 export EDITOR=vim
 export BROWSER=firefox
 
+export OPENBOX_TERMINAL=sakura
+export OPENBOX_EDITOR=gvim
+export OPENBOX_BROWSER=firefox
+export OPENBOX_EMAIL=thunderbird
+
 eval $(dircolors ~/.shell/database/colors.ls)
 
-export MAN_PATH=~/hacking/installs/man
-export PYTHONPATH=~/.local/lib/python2.7/site-packages:~/hacking/libraries:~/research/software/libraries
-export CLASSPATH=.:./bin/:usr/share/java
+export PYTHONPATH=~/research/software/third_party/imp/build/lib
+export CLASSPATH=.:./bin/:usr/share/java:/home/kale/hacking/projects/android-sdk/tools
 export TEXINPUTS=~/hacking/config/latex:
 export BSTINPUTS=~/hacking/config/latex:
 
