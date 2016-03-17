b9db22800e8117245994649c06a492a2be1a0ead
Kale Kundert <kale@thekunderts.net>
2016-03-17 15:14:50 -0700

    Alphabetize the symlinks.

--- install.d/20.make_links.sh
+++ install.d/20.make_links.sh
@@ -12,10 +12,11 @@ if [ $EUID -ne 0 ]; then
     mkdir -p ~/.config
     mkdir -p ~/.config/sakura
 
+    ln -nsf $INSTALL_DIR/resources/cookiecutterrc ~/.cookiecutterrc
+    ln -nsf $INSTALL_DIR/resources/gitignore ~/.gitignore
+    ln -nsf $INSTALL_DIR/resources/inputrc ~/.inputrc
+    ln -nsf $INSTALL_DIR/resources/pypirc ~/.pypirc
+    ln -nsf $INSTALL_DIR/resources/sakura.conf ~/.config/sakura/
     ln -nsf $INSTALL_DIR/resources/user-dirs.dirs ~/.config/
     ln -nsf $INSTALL_DIR/resources/user-dirs.locale ~/.config/
-    ln -nsf $INSTALL_DIR/resources/sakura.conf ~/.config/sakura/
-    ln -nsf $INSTALL_DIR/resources/gitignore ~/.gitignore
-    ln -nsf $INSTALL_DIR/resources/cookiecutterrc ~/.cookiecutterrc
-    ln -nsf $INSTALL_DIR/resources/pypirc ~/.pypirc
 fi
