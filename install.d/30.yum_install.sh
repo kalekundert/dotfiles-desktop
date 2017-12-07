30cde7da69fc333d8347a7dcf2951c577ed93fe9
Kale Kundert <kale@thekunderts.net>
2017-12-07 13:29:56 -0800

    Install some more packages.

--- install.d/30.yum_install.sh
+++ install.d/30.yum_install.sh
@@ -1,7 +1,10 @@
 if [ -e /etc/fedora-release -a $EUID -ne 0 ]; then
+    echo "Please provide a sudo password to install packages:"
+    sudo dnf update -y
     sudo dnf install -y                                     \
         http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm          \
-        http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm    \
+        http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y                                     \
         "@Development Tools"                                \
         "@RPM Development Tools"                            \
         "@C Development Tools and Libraries"                \
@@ -9,20 +12,29 @@ if [ -e /etc/fedora-release -a $EUID -ne 0 ]; then
         acpi                                                \
         autofs                                              \
         calibre                                             \
+        clang                                               \
         duplicity                                           \
+        eom                                                 \
         firefox                                             \
+        foomatic                                            \
         fuse-sshfs                                          \
         gimp                                                \
+        git                                                 \
+        gmrun                                               \
         gnumeric                                            \
         htop                                                \
+        hplip                                               \
         hub                                                 \
         inkscape                                            \
         lapack-devel                                        \
         libreoffice                                         \
         libreoffice-TexMaths                                \
         lxrandr                                             \
+        lxtask                                              \
         mailx                                               \
+        ncurses-devel                                       \
         pithos                                              \
+        pinentry-gtk                                        \
         pstoedit                                            \
         pymol                                               \
         python3-devel                                       \
@@ -31,11 +43,11 @@ if [ -e /etc/fedora-release -a $EUID -ne 0 ]; then
         python3-numpy                                       \
         python3-pandas                                      \
         python3-virtualenv                                  \
-        qiv                                                 \
         sakura                                              \
         sendmail                                            \
         scribus                                             \
         scrot                                               \
+        tidy                                                \
         thunderbird                                         \
         thunderbird-enigmail                                \
         thunderbird-lightning-gdata                         \
@@ -47,5 +59,9 @@ if [ -e /etc/fedora-release -a $EUID -ne 0 ]; then
         zathura                                             \
         zathura-plugins-all                                 \
         zsh                                                 \
+
+    # Get Microsoft fonts (for snapgene)
+    sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+    sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 fi
