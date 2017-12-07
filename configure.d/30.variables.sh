e483ac038ab6d479673166b902b0984c8469da4a
Kale Kundert <kale@thekunderts.net>
2017-12-07 13:27:11 -0800

    Calculate $PATH and $PYTHONPATH more deliberately.

--- configure.d/30.variables.sh
+++ configure.d/30.variables.sh
@@ -1,30 +1,38 @@
-if [ -z "$PATH_CONFIGURED" ]; then
-    export PATH_CONFIGURED='yes'
-    export PATH="/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin"
-    if [ -d ~/.local/bin ]; then
-        for SUBDIR in ~/.local/bin ~/.local/bin/*; do
-            if [[ -d $SUBDIR ]]; then
-                export PATH=$SUBDIR:$PATH
-            fi
+function add_dir_to_path () {
+    # Usage:
+    #   add_dirs_to_path <dir>
+    if [ -d "$1" ]; then
+        export PATH=$1:$PATH
+    fi
+}
+
+function add_subdirs_to_path () {
+    # Usage:
+    #   add_subdirs_to_path <find args>
+    if [ -d $1 ]; then
+        for SUBDIR in $(find "$@" -type d | sort); do
+            add_dir_to_path $SUBDIR
         done
     fi
-    export PATH="$HOME/.shell/scripts:$PATH"
-    export MANPATH="$MANPATH:$HOME/.local/man:$HOME/.local/share/man"
-fi
+}
+
+export PATH="/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin"
+
+add_dir_to_path ~/.local/bin
+add_dir_to_path ~/.shell/scripts
+add_subdirs_to_path ~/.virtualenvs -maxdepth 2 -wholename '*python??/bin'
 
 export EDITOR=vim
 export BROWSER=firefox
 
-export OPENBOX_TERMINAL=sakura
-export OPENBOX_EDITOR=gvim
-export OPENBOX_BROWSER=firefox
-export OPENBOX_EMAIL=thunderbird
-
 eval $(dircolors ~/.shell/configure.d/resources/colors.ls)
 
+export CC=/bin/clang
+export CXX=/bin/clang++
 export LD_LIBRARY_PATH=~/.local/lib:~/.local/lib64
 export PKG_CONFIG_PATH=~/.local/lib/pkgconfig
-export PYTHONPATH=~/research/software/third_party/imp/build/lib
+#export MANPATH=~/.local/man:~/.local/share/man
+#export PYTHONPATH=~/research/software/third_party/imp/build/lib
 export PYTHONSTARTUP=~/.pythonrc
 export CLASSPATH=.:./bin/:usr/share/java:/home/kale/hacking/third_party:/home/kale/hacking/projects/android-sdk/tools
 export TEXINPUTS=~/hacking/config/latex:
@@ -35,3 +43,7 @@ export AUTOSSH_POLL=5
 export AUTOSSH_PIDFILE=~/.ssh/autossh.pid
 
 export GPG_TTY=`tty`
+
+source ~/.virtualenvs/python36/bin/virtualenvwrapper.sh
+export WORKON_DIR=~/.virtualenvs
+
