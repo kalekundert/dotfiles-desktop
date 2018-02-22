f321d0556dcbe7ff7ddd07a530d194e25356c174
Kale Kundert <kale@thekunderts.net>
2018-02-22 10:30:48 -0800

    Add the exmemo aliases.

--- configure.d/40.aliases.sh
+++ configure.d/40.aliases.sh
@@ -146,6 +146,29 @@ alias bfg='java -jar ~/hacking/third_party/bfg-1.12.16.jar'
     #alias g$ga="git $ga"
 #done
 
+# Exmemo {{{1
+# ======
+alias en='exmemo note new'
+alias ee='exmemo note edit'
+alias eo='exmemo note open'
+alias eb='exmemo note build'
+alias el='exmemo note ls'
+alias ew='exmemo note browse'
+alias eww='exmemo note browse -w'
+alias eps='exmemo protocol show'
+alias epp='exmemo protocol print'
+alias epe='exmemo protocol edit'
+alias epl='exmemo protocol ls'
+alias epf='exmemo protocol save'
+alias edy='exmemo data sync'
+alias edk='exmemo data link'
+alias edg='exmemo data gel'
+
+function ed () {
+    d=$(exmemo note directory "$@")
+    [ $? = 0 ] && cd $d || echo $d  # Don't try to cd if something goes wrong.
+}
+
 # Python {{{1
 # ======
 alias py=python3
