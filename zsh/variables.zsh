8f13b2f29df88095ba0ae9bfde65f9bbeac517ef
Kale Kundert <kale@thekunderts.net>
2020-01-02 16:53:12 -0800

    Use the Agkozak zsh prompt.

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -12,4 +12,10 @@ export NINJA_STATUS="[%f/%t] [-j%r] "
 export NVIM_GTK_NO_HEADERBAR=1
 export TEXMFHOME="$HOME/.config/texlive"
 
-export YSU_IGNORED_ALIASES=("l" "s")
+export YSU_IGNORED_ALIASES=("l" "s" "cd..")
+export AGKOZAK_USER_HOST_DISPLAY=0
+export AGKOZAK_BLANK_LINES=1
+export AGKOZAK_PROMPT_CHAR=( '$' '#' ':' )
+export AGKOZAK_PROMPT_CHAR=( ❯ '%F{red}❯%f' ❮ )
+export AGKOZAK_CUSTOM_SYMBOLS=( '⇣⇡' '⇣' '⇡' '+' 'x' '!' '»' '?' )
+
